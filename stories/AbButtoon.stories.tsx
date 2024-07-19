49bae9e97b69a16a514bd7c84861d7991d7a4801
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-07-19 15:51:20 -0300

    first commit

---
+++ stories/AbButtoon.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { AbButton } from '../src';
+
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+export default {
+  title: 'Componnents/AbButton',
+  component: AbButton,
+} as ComponentMeta<typeof AbButton>;
+
+const Template: ComponentStory<typeof AbButton> = () => <AbButton />;
+
+export const Primary = Template.bind({});
