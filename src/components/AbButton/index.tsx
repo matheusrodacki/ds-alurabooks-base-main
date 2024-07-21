90f901595f66aeb369bf81c42df799351f6c45f0
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-07-21 12:13:29 -0300

    chore: Update dependencies and add styled-components to AbButton component

--- src/components/AbButton/index.tsx
+++ src/components/AbButton/index.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background: #eb9b00;
+  padding: 10px 32px;
+  border: 2px solid #eb9b00;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+  &:hover {
+    background: #b87900;
+    border: 2px solid #b87900;
+  }
+`;
 
 export const AbButton = () => {
-  return <button>Clique Aqui</button>;
+  return <StyledButton>Clique Aqui</StyledButton>;
 };
